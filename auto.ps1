a10f07f088b129cc1204cbe54610c7b29355195d
Stephen Gillie <stephengillie@gmail.com>
2023-02-03 12:41:10 -0800

    First commit of automation.

---
+++ auto.ps1
@@ -0,0 +1,9 @@
+#Copyright 2023 Gilgamech Technologies
+#Author: Stephen Gillie
+#Created on: 2/3/2023
+#Updated on: 2/3/2023
+#Notes: 
+
+$Logtime = (get-date -f s).replace(":","").replace("-","");
+#winget upgrade --all --log "C:\repos\GTAutomation\logs\$Logtime.log"
+winget upgrade --all | out-file "C:\repos\GTAutomation\logs\$Logtime.log"
