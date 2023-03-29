dadf374c1e92fe68304cb517dae5edd636cb484d
GILNOVOX\Gilgamech <Gilgamech@Gilgamech.com>
2023-03-29 06:16:39 -0800

    #1.2: Add automatic virus scan. Also revert name change from "auto.ps1" to "winget upgrade.ps1".

--- auto.ps1
+++ auto.ps1
@@ -1,9 +1,22 @@
 #Copyright 2023 Gilgamech Technologies
 #Author: Stephen Gillie
-#Created on: 2/3/2023
-#Updated on: 2/3/2023
-#Notes: 
+#Title: auto.ps1 v1.2
+#Created on: 3/18/2023
+#Updated on: 3/28/2023
+#Update history: 
+#1.0: Original release.
+#1.1: Add automatic drive optimization.
+#1.2: Add automatic virus scan.
 
-$Logtime = (get-date -f s).replace(":","").replace("-","");
+Import-Module C:\repos\GTAutomation\Add-AudioType.ps1 -Force
 #winget upgrade --all --log "C:\repos\GTAutomation\logs\$Logtime.log"
-winget upgrade --all --disable-interactivity | out-file "C:\repos\GTAutomation\logs\$Logtime.log"
+$out = winget upgrade --all --disable-interactivity 
+Add-Log "winget upgrade: `n$out"
+
+(Get-PSDrive).Name -match '^[a-z]$'|%{
+	Optimize-Volume -DriveLetter $_ -Defrag -ReTrim
+	Add-Log "Drive $_ optimized"
+}
+
+Update-MpSignature;
+Start-MpScan;
